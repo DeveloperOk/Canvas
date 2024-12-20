bcbe99e2eed990d12f4968800975ffbbdd659f68
DeveloperOk <ok.fahrettin@gmail.com>
2024-12-20 16:53:06 +0300

    Canvas was added.

--- app/src/main/java/com/enterprise/canvas/MainActivity.kt
+++ app/src/main/java/com/enterprise/canvas/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.enterprise.canvas.ui.theme.CanvasTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +26,42 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CanvasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CanvasApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun CanvasApp() {
+    Scaffold(modifier = Modifier.systemBarsPadding().fillMaxSize()) { innerPadding ->
+        val viewModel = viewModel<DrawingViewModel>()
+        val state by viewModel.state.collectAsStateWithLifecycle()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CanvasTheme {
-        Greeting("Android")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            DrawingCanvas(
+                paths = state.paths,
+                currentPath = state.currentPath,
+                onAction = viewModel::onAction,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            )
+            CanvasControls(
+                selectedColor = state.selectedColor,
+                colors = allColors,
+                onSelectColor = {
+                    viewModel.onAction(DrawingAction.OnSelectColor(it))
+                },
+                onClearCanvas = {
+                    viewModel.onAction(DrawingAction.OnClearCanvasClick)
+                }
+            )
+        }
     }
 }
